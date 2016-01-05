c007227cc62f6a796818d1d923e87563598513de
Michael Choe <michael@bighuman.com>
2016-01-05 11:15:18 -0500

    Initial Commit

---
+++ Resplendent/Resplendent.h
@@ -0,0 +1,19 @@
+//
+//  Resplendent.h
+//  Resplendent
+//
+//  Created by Michael Choe on 1/5/16.
+//  Copyright © 2016 Strauss LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Resplendent.
+FOUNDATION_EXPORT double ResplendentVersionNumber;
+
+//! Project version string for Resplendent.
+FOUNDATION_EXPORT const unsigned char ResplendentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Resplendent/PublicHeader.h>
+
+
