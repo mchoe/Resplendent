99d4a21aed43a6d5caf6a7c4f8d14b55d7843288
Michael Choe <michael@bighuman.com>
2016-01-06 11:32:15 -0500

    Cleanup

--- ResplendentTests/UIColorTests.swift
+++ ResplendentTests/UIColorTests.swift
@@ -33,16 +33,6 @@ import XCTest
 
 class UIColorTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testUIColorInitialization() {
         
         let testColor = UIColor(hexString: "#bba528")
